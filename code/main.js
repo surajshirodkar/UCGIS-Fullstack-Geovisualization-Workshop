bb711fe41583da7c47d62b371652928723eb9676
surajshirodkar <85908647+surajshirodkar@users.noreply.github.com>
2021-06-14 20:02:47 -0400

    Update main.js
    Update Syntax

--- code/main.js
+++ code/main.js
@@ -49,7 +49,7 @@ var popup = L.popup();
 
 function onMapClick(event) {
     popup.setLatLng(event.latlng)
-        .setContent("Suraj things you clicked the map at " + event.latlng.toString())
+        .setContent("Suraj thinks you clicked the map at " + event.latlng.toString())
         .openOn(mymap);
 }
 
